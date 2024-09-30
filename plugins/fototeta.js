c8cdf72564e0492f68e93606bfc3600ceee4cd1c
SNT Oficial <124302047+TIKIof@users.noreply.github.com>
2024-09-30 18:36:18 -0400

    Update fototeta.js

--- plugins/fototeta.js
+++ plugins/fototeta.js
@@ -5,5 +5,5 @@ let mentionedJid = [who]
 conn.send3Button(m.chat, `
 Padre nuestro, que estás en los Cielos, santificado sea tu nombre, venga tu Reino, hágase tu voluntad así en la tierra como en el cielo. y perdónanos nuestras deudas así como nosotros perdonamos a nuestros deudores, y no nos dejes caer en la tentación, mas líbranos del mal.
 `.trim(), 'Bot Tiburón 🦈| Black bot command', '🔷️ MENÚ 🔷', `${usedPrefix}menu`, '👻 Run Time 👻', `${usedPrefix}runtime`, '💠️ MENÚ AUDIOS 💠️', `${usedPrefix}menu2`, m, false, { contextInfo: { mentionedJid }})}
-handler.command = /^(estado|status|estate|state|stado|stats)$/i
+handler.command = /^(oracion|padre nuestro|padrenuestro)$/i
 module.exports = handler
